e6f8b7c6bde3bf6bc388895a204f55d16e097cb5
Joseph Nixon <=>
2023-12-02 18:30:44 -0500

    Added solution to day 2 part 2

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AdventOfCode.Puzzles._2023.Day02;
 
-var solution = new CubeSolution1();
+var solution = new CubeSolution2();
 Console.WriteLine(solution.Solve());
---
+++ AdventOfCode/Puzzles/2023/Day02/CubeSolution2.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode.Puzzles._2023.Day02
+{
+    public class CubeSolution2 : CubeBase
+    {
+        public override object Solve()
+        {
+            var cubeGames = ReadInputFromFile();
+            var sum = 0;
+            foreach (var game in cubeGames)
+            {
+                var bluePower = 0;
+                var redPower = 0;
+                var greenPower = 0;
+                foreach( var gameCubeSet in game.CubeSets ) {
+                    if (gameCubeSet.ContainsKey(Color.blue)) { bluePower = Math.Max(bluePower, gameCubeSet[Color.blue]); }
+                    if (gameCubeSet.ContainsKey(Color.red)) { redPower = Math.Max(redPower, gameCubeSet[Color.red]); }
+                    if (gameCubeSet.ContainsKey(Color.green)) { greenPower = Math.Max(greenPower, gameCubeSet[Color.green]); }
+                }
+                sum += (bluePower * redPower * greenPower);
+            }
+            return sum;
+        }
+    }
+}

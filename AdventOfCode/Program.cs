70759c4920b4af306234430d91c512a798d76950
Joseph Nixon <=>
2023-12-06 00:29:40 -0500

    Added solution to part 2 of day 6

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AdventOfCode.Puzzles._2023.Day06;
 
-var solution = new RaceSolution1();
+var solution = new RaceSolution2();
 Console.WriteLine(solution.Solve());
---
+++ AdventOfCode/Puzzles/2023/Day06/RaceSolution2.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode.Puzzles._2023.Day06
+{
+    internal class RaceSolution2 : BaseSolution<Race>
+    {
+        internal override Race ReadInputFromFile()
+        {
+            var lines = File.ReadAllLines(@"./Puzzles/2023/Day06/Input.txt");
+            var time = 0L;
+            var distance = 0L;
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("Time: "))
+                {
+                    time = long.Parse(string.Join("", line[6..].Split(' ', StringSplitOptions.RemoveEmptyEntries)));
+                }
+                else if (line.StartsWith("Distance: "))
+                {
+                    distance = long.Parse(string.Join("", line[10..].Split(' ', StringSplitOptions.RemoveEmptyEntries)));
+                }
+            }
+            return new Race { Time = time, Distance = distance };
+        }
+
+        public override object Solve()
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            var race = ReadInputFromFile();
+
+            var raceWinOptions = 0;
+            var timeCharge = 1;
+            while (timeCharge < race.Time)
+            {
+                if (timeCharge * (race.Time - timeCharge) > race.Distance)
+                {
+                    raceWinOptions++;
+                }
+                timeCharge++;
+            }
+
+            timer.Stop();
+            Console.WriteLine($"Solved in {timer.ElapsedMilliseconds}ms");
+            return raceWinOptions;
+        }
+    }
+}

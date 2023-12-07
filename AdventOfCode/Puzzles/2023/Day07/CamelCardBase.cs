3b19d19e58ba7dc11820805b55f9dcf48e01f8ef
Joseph Nixon <=>
2023-12-07 01:31:42 -0500

    Added comments to camelCardHand class

--- AdventOfCode/Puzzles/2023/Day07/CamelCardBase.cs
+++ AdventOfCode/Puzzles/2023/Day07/CamelCardBase.cs
@@ -25,6 +25,9 @@ namespace AdventOfCode.Puzzles._2023.Day07
 
     public class CamelCardHand : IComparable
     {
+        /// <summary>
+        /// The default value of each card.
+        /// </summary>
         private static ReadOnlyDictionary<char, int> _cardValues = new ReadOnlyDictionary<char, int>(new Dictionary<char, int>
         {
             { 'A', 14 },
@@ -44,6 +47,9 @@ namespace AdventOfCode.Puzzles._2023.Day07
 
         private const char JOKER = 'J';
 
+        /// <summary>
+        /// When enabled, 'J' is treated as a wildcard and can be used to complete any hand.
+        /// </summary>
         private readonly bool _jokerMode;
 
         public CamelCardHand(char[] cards, int bid, bool jokerMode)
@@ -53,72 +59,71 @@ namespace AdventOfCode.Puzzles._2023.Day07
             _jokerMode = jokerMode;
         }
 
+        /// <summary>
+        /// Base number of points bid for this hand.
+        /// </summary>
         public int Bid { get; set; }
+        /// <summary>
+        /// Collection of cards in the hand.
+        /// </summary>
         public char[] Cards { get; set; }
 
+        /// <summary>
+        /// the hand's strength for comparing to other hands.
+        /// </summary>
         private Strength _strength = Strength.None;
 
         public Strength Strength
         {
             get
             {
-                if (_strength == Strength.None)
-                {
-                    _strength = GetStrength();
-                }
+                // Lazy load the strength.
+                if (_strength == Strength.None) { _strength = GetStrength(); }
                 return _strength;
             }
             private set { _strength = value; }
         }
 
-        private int GetCardValue(char card)
-        {
-            if (_jokerMode && card == JOKER)
-            {
-                return 1;
-            }
-            else
-            {
-                return _cardValues[card];
-            }
-        }
+        /// <summary>
+        /// Return value of card. If in joker mode, 'J' is treated as a wildcard and returns 1.
+        /// </summary>
+        /// <param name="card"></param>
+        /// <returns></returns>
+        private int GetCardValue(char card) => _jokerMode && card == JOKER ? 1 : _cardValues[card];
 
+        /// <summary>
+        /// Custom CompareTo override thand handles comparing hands based on strength and card values.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public int CompareTo(object? obj)
         {
             if (obj is CamelCardHand compHand)
             {
-                if (compHand.Strength == Strength)
+                var handComp = Strength.CompareTo(compHand.Strength);
+                if (handComp == 0)
                 {
                     for (int i = 0; i < Cards.Length; i++)
                     {
-                        if (GetCardValue(Cards[i]) > GetCardValue(compHand.Cards[i]))
+                        var comp = GetCardValue(Cards[i]).CompareTo(GetCardValue(compHand.Cards[i]));
+                        if (comp != 0)
                         {
-                            return 1;
-                        }
-                        else if (GetCardValue(Cards[i]) < GetCardValue(compHand.Cards[i]))
-                        {
-                            return -1;
+                            return comp;
                         }
                     }
-                    return 0; // if all are the same.
                 }
-                else if (compHand.Strength > Strength)
-                {
-                    return -1;
-                }
-                else
-                {
-                    return 1;
-                }
+                return handComp;
             }
-            else
-            {
-                return 1;
-            }
+            return 1;
         }
 
+        /// <summary>
+        /// Gets the strength of the hand.
+        /// </summary>
+        /// <returns></returns>
         private Strength GetStrength()
         {
+            // Group cards by value.
             var groups = Cards.GroupBy(x => x);
             if (groups.Any(x => x.Count() == 5))
             {
@@ -126,6 +131,7 @@ namespace AdventOfCode.Puzzles._2023.Day07
             }
             else if (groups.Any(x => x.Count() == 4))
             {
+                // if joker mode is enabled, and there's at least one joker, 4 of a kind is actually 5 of a kind.
                 if (_jokerMode && Cards.Any(c => c == JOKER))
                 {
                     return Strength.FiveOfAKind;
@@ -134,6 +140,8 @@ namespace AdventOfCode.Puzzles._2023.Day07
             }
             else if (groups.Any(x => x.Count() == 3) && groups.Any(x => x.Count() == 2))
             {
+                // if joker mode is enabled, and there's at least one joker, full house is actually 5 of a kind.
+                // because 444JJ or 44JJJ would both be 5 of a kind.
                 if (_jokerMode && Cards.Any(c => c == JOKER))
                 {
                     return Strength.FiveOfAKind;
@@ -142,24 +150,19 @@ namespace AdventOfCode.Puzzles._2023.Day07
             }
             else if (groups.Any(x => x.Count() == 3))
             {
+                // if joker mode is enabled
                 if (_jokerMode)
                 {
                     var jokerCount = Cards.Count(x => x == JOKER);
+                    // if there's 3 jokers the only options is four of a kind.
                     if (jokerCount == 3)
                     {
-                        if (groups.Count() == 2)
-                        {
-                            return Strength.FiveOfAKind;
-                        }
-                        else
-                        {
-                            return Strength.FourOfAKind;
-                        }
+                        return Strength.FourOfAKind;
                     }
-                    else if (jokerCount == 2)
-                    {
-                        return Strength.FiveOfAKind;
-                    }
+                    //else if (jokerCount == 2) // not possible, would fall in full house logic
+                    //{
+                    //    return Strength.FiveOfAKind;
+                    //}
                     else if (jokerCount == 1)
                     {
                         return Strength.FourOfAKind;
@@ -169,14 +172,16 @@ namespace AdventOfCode.Puzzles._2023.Day07
             }
             else if (groups.Count(x => x.Count() == 2) == 2)
             {
+                // if joker mode is enabled
                 if (_jokerMode)
                 {
                     var jokerCount = Cards.Count(x => x == JOKER);
+                    // if there's 3 jokers the only options is four of a kind, because one of the pairs is a joker pair.
                     if (jokerCount == 2)
                     {
                         return Strength.FourOfAKind;
                     }
-                    else if (jokerCount == 1)
+                    else if (jokerCount == 1) // if it's 1, then it's a full house, because there's 2 pairs and 1 joker.
                     {
                         return Strength.FullHouse;
                     }
@@ -185,14 +190,11 @@ namespace AdventOfCode.Puzzles._2023.Day07
             }
             else if (groups.Any(x => x.Count() == 2))
             {
+                // if joker mode is enabled
                 if (_jokerMode)
                 {
-                    var jokerCount = Cards.Count(x => x == JOKER);
-                    if (jokerCount == 2)
-                    {
-                        return Strength.ThreeOfAKind;
-                    }
-                    else if (jokerCount == 1)
+                    // if there are any jokers, the only remaining option is three of a kind.
+                    if (Cards.Any(x => x == JOKER))
                     {
                         return Strength.ThreeOfAKind;
                     }
@@ -201,8 +203,10 @@ namespace AdventOfCode.Puzzles._2023.Day07
             }
             else
             {
+                // if joker mode is enabled
                 if (_jokerMode)
                 {
+                    // any jokers means it's a pair.
                     if (Cards.Count(x => x == JOKER) > 0)
                     {
                         return Strength.OnePair;
